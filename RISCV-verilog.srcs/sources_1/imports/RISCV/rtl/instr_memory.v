34effc221e84adc551414727a4888492968eb376
KaushikBalaji <kaushikbalaji0609@outlook.com>
2025-12-13 15:40:26 +0530

    Previous project simulation issue, so created new project.

---
+++ RISCV-verilog.srcs/sources_1/imports/RISCV/rtl/instr_memory.v
@@ -0,0 +1,27 @@
+`timescale 1ns/1ps
+
+module instr_mem(input wire[31:0] addr,
+                 output wire[31:0] instr);
+    
+    reg[31:0] mem[0:2048];
+    
+    integer read, i;
+    initial begin
+        // pad addresses with zeros for mem not filled by the file .... this removes xxxx values
+        for (i = 0; i < 2048; i = i + 1) begin
+            mem[i] = 32'b0;
+        end
+        $display("Loading hex file ... INSTR mem module");
+        $readmemh("instr_raw.hex", mem);
+//        if(!read)
+//        	$display("Could not find the 1-even.hex file");
+        
+        
+        $display("Finished loading hex. First words:");
+        for (i = 0; i < 22; i = i + 1) $display(" mem[%0d] = %08x", i, mem[i]);
+    end
+    
+    assign instr = mem[addr[31:2]];
+    
+endmodule
+    
